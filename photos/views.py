e9b9b7e03167ee1679f4fbac76f6a14ceb55d251
davidjj76 <davidjj76@gmail.com>
2017-05-02 01:46:52 +0200

    Load home page with a django template

--- photos/views.py
+++ photos/views.py
@@ -1,12 +1,11 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 from photos.models import Photo
 
 
 def home(request):
     photos = Photo.objects.all()
-    html = '<ul>'
-    for photo in photos:
-        html += '<li>' + photo.name + '</li>'
-    html += '</ul>'
-    return HttpResponse(html)
+    context = {
+        'photos_list': photos,
+    }
+    return render(request, 'photos/home.html', context)
