e895e954d6dba00029a69f44033df8b9d6aceb8d
davidjj76 <davidjj76@gmail.com>
2017-05-02 02:10:14 +0200

    Add owner to photo model and make migration

--- photos/models.py
+++ photos/models.py
@@ -1,8 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 COPYRIGHT = 'RIG'
 COPYLEFT = 'LEF'
 CREATIVE_COMMONS = 'CC'
+
+PUBLIC = 'PUB'
+PRIVATE = 'PRI'
 
 LICENSES = (
     (COPYRIGHT, 'Copyright'),
@@ -10,14 +14,21 @@ LICENSES = (
     (CREATIVE_COMMONS, 'Creative Commons'),
 )
 
+VISIBILITY = (
+    (PUBLIC, 'Public'),
+    (PRIVATE, 'Private'),
+)
+
 class Photo(models.Model):
 
+    owner = models.ForeignKey(User)
     name = models.CharField(max_length=150)
     url = models.URLField()
     description = models.TextField(blank=True, null=True, default='')
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
     license = models.CharField(max_length=3, choices=LICENSES)
+    visibility = models.CharField(max_length=3, choices=VISIBILITY, default=PUBLIC)
 
     def __str__(self):
         return self.name
