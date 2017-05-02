1434a080b9dfa79af7d3855c08f9833fbdb02d76
davidjj76 <davidjj76@gmail.com>
2017-05-03 00:12:10 +0200

    Implement create photo with model form

--- frikr/urls.py
+++ frikr/urls.py
@@ -1,13 +1,19 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from photos.views import home, detail
+from photos.views import home, detail, create
 from users.views import login, logout
 
 urlpatterns = [
+    # Admin
     url(r'^admin/', admin.site.urls),
+
+    # Photos
     url(r'^$', home, name='photos_home'),
     url(r'^photos/(?P<pk>[0-9]+)$', detail, name='photo_detail'),
+    url(r'^photos/new$', create, name='photo_create'),
+
+    # Users
     url(r'^login$', login, name='users_login'),
     url(r'^logout$', logout, name='users_logout'),
 ]
--- photos/views.py
+++ photos/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
+from django.urls import reverse
 
+from photos.forms import PhotoForm
 from photos.models import Photo, PUBLIC
 
 
@@ -22,3 +24,23 @@ def detail(request, pk):
         return render(request, 'photos/detail.html', context)
     else:
         return HttpResponseNotFound('No existe la foto!!!')
+
+
+def create(request):
+    success_message = ''
+    if request.method == 'GET':
+        form = PhotoForm()
+    else:
+        form = PhotoForm(request.POST)
+        if form.is_valid():
+            new_photo = form.save()
+            form = PhotoForm()
+            success_message = 'Guardado con éxito! '
+            success_message += '<a href="{0}">'.format(reverse('photo_detail', args=[new_photo.pk]))
+            success_message += 'Ver foto'
+            success_message += '</a>'
+    context = {
+        'form': form,
+        'success_message': success_message,
+    }
+    return render(request, 'photos/new_photo.html', context)
