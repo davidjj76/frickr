87491e70a010aba299d5ede13f14f7831e24b2c4
davidjj76 <davidjj76@gmail.com>
2017-05-02 22:06:31 +0200

    Create users app

--- frikr/settings.py
+++ frikr/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'frikr',
     'photos.apps.PhotosConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
--- frikr/urls.py
+++ frikr/urls.py
@@ -2,9 +2,12 @@ from django.conf.urls import url
 from django.contrib import admin
 
 from photos.views import home, detail
+from users.views import login, logout
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', home, name='photos_home'),
     url(r'^photos/(?P<pk>[0-9]+)$', detail, name='photo_detail'),
+    url(r'^login$', login, name='users_login'),
+    url(r'^logout$', logout, name='users_logout'),
 ]
