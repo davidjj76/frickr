70ac8a60c41150ea9c439d406c83402a343396b7
davidjj76 <davidjj76@gmail.com>
2017-05-04 01:12:10 +0200

    Create endpoint for list users with API View

--- users/api.py
+++ users/api.py
@@ -1,20 +1,16 @@
 from django.contrib.auth.models import User
-from django.http import HttpResponse
-from django.views import View
-from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from users.serializers import UserSerializer
 
 
-class UserListAPI(View):
+class UserListAPI(APIView):
 
     def get(self, request):
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
-        serialized_users = serializer.data
-        renderer = JSONRenderer()
-        json_users = renderer.render(serialized_users)
-        return HttpResponse(json_users)
+        return Response(serializer.data)
 
     def post(self, request):
         pass
