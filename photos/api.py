255139173160fb2e705a18e440a58657ccc69435
davidjj76 <davidjj76@gmail.com>
2017-05-05 00:10:57 +0200

    Establish permissions in photos API

--- photos/api.py
+++ photos/api.py
@@ -1,4 +1,5 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from photos.models import Photo
 from photos.serializers import PhotoSerializer, PhotoListSerializer
@@ -8,6 +9,7 @@ class PhotoListAPI(ListCreateAPIView):
 
     queryset = Photo.objects.all()
     serializer_class = PhotoListSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly, )
 
     def get_serializer_class(self):
         return PhotoSerializer if self.request.method == 'POST' else self.serializer_class
@@ -16,4 +18,5 @@ class PhotoListAPI(ListCreateAPIView):
 class PhotoDetailAPI(RetrieveUpdateDestroyAPIView):
 
     queryset = Photo.objects.all()
+    permission_classes = (IsAuthenticatedOrReadOnly, )
     serializer_class = PhotoSerializer
